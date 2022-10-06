8b33cdc75055b84692f9652984c3dd2edc762db4
dopaminegirl19 <morganemoss@gmail.com>
2022-10-05 20:49:16 -0400

    add 2nd contact field for users and item descriptions

--- data-access/create-tables.sql
+++ data-access/create-tables.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS userships;
 USE userships;
 
-DROP TABLE IF EXISTS assets;
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS users;
 
@@ -10,17 +9,23 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(128) NOT NULL,
     contact VARCHAR(128) NOT NULL,
+    contact2 VARCHAR(128),
     PRIMARY KEY (`id`)
 );
 
 INSERT INTO users
-    (name, contact)
+    (name, contact, contact2)
 VALUES
-    ('Morgane', 'morgane@usership.go');
+    ('Morgane', 'morgane@usership.go', '07904663791'),
+    ('Laurence', 'laurence@usership.go', NULL),
+    ('Andrew', 'andrew@usership.go', NULL),
+    ('Cyril', 'cyril@usership.go', NULL),
+    ('Julia', 'julia@usership.go', NULL);
 
 CREATE TABLE items (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(128) NOT NULL,
+    description VARCHAR(255),
     current_user_id INT NOT NULL,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`current_user_id`) REFERENCES users(`id`)
