3b362e84af634ec6285260e8c69baa5b453b4418
dopaminegirl19 <morganemoss@gmail.com>
2022-10-10 09:26:23 -0400

    update package imports, rename to dbConnect

--- main.go
+++ main.go
@@ -1,45 +1,4 @@
-// Written according to the tutorial at: https://go.dev/doc/tutorial/database-access
-
 package main
 
-import (
-	"github.com/go-sql-driver/mysql"
-
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
-)
-
-var db *sql.DB
-
-func main() {
-	// Capture connection properties.
-	cfg := mysql.Config{
-		User:                 os.Getenv("DBUSER"),
-		Passwd:               os.Getenv("DBPASS"),
-		Net:                  "tcp",
-		Addr:                 "127.0.0.1:3306",
-		DBName:               "userships",
-		AllowNativePasswords: true,
-	}
-	// Get a database handle.
-	var err error
-	db, err = sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	pingErr := db.Ping()
-	if pingErr != nil {
-		log.Fatal(pingErr)
-	}
-	fmt.Println("Connected!")
-
-	users, err := UsersByName("Morgane")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Users found: %v\n", users)
-}
+var groupUsers []User
+var groupItems []Item
